c053bec9b70d1c9a3e5472cf962909b33c5b1e08
hoangnh <hoangnn@justgola.com>
2018-06-01 15:09:13 +0700

    Init table

--- app/src/main/java/nguyenhuu/hoang/powerstationidentifier/MainActivity.kt
+++ app/src/main/java/nguyenhuu/hoang/powerstationidentifier/MainActivity.kt
@@ -1,11 +1,12 @@
 package nguyenhuu.hoang.powerstationidentifier
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import kotlinx.android.synthetic.main.activity_main.*
 import nguyenhuu.hoang.powerstationidentifier.models.Distance
 import nguyenhuu.hoang.powerstationidentifier.models.Station
+import nguyenhuu.hoang.powerstationidentifier.models.TableViewAdapter
 
 class MainActivity : BaseActivity() {
 
@@ -20,5 +21,21 @@ class MainActivity : BaseActivity() {
         val distancesJson = inputStreamDistances.bufferedReader().use { it.readText() }
         val stations: ArrayList<Station> = Gson().fromJson(stationsJson, object : TypeToken<ArrayList<Station>>() {}.type)
         val distances: ArrayList<Distance> = Gson().fromJson(distancesJson, object : TypeToken<ArrayList<Distance>>() {}.type)
+
+        // display on tableview
+        val columnHeaderList = arrayListOf<String>("Đường dây", "Vị trí", "Khoảng cột", "Cộng dồn", "Cộng dồn ngược")
+        val rowHeaderList = IntArray(distances.size, { i -> i + 1 }).map { i -> i.toString() }
+        val cellList = initCellList(distances)
+        val adapter = TableViewAdapter(this)
+        table.adapter = adapter
+        adapter.setAllItems(columnHeaderList, rowHeaderList, cellList)
+    }
+
+    fun initCellList(distances: ArrayList<Distance>): List<List<String>> {
+        val result = emptyArray<List<String>>().toMutableList()
+        distances.forEachIndexed { _, distance ->
+            result.add(arrayOf<String>(distance.LineName, distance.Position, distance.Distance.toString(), distance.Incremental.toString(), distance.ReversedIncremental.toString()).toList())
+        }
+        return result
     }
 }
--- app/src/main/java/nguyenhuu/hoang/powerstationidentifier/models/Models.kt
+++ app/src/main/java/nguyenhuu/hoang/powerstationidentifier/models/Models.kt
@@ -1,7 +1,85 @@
 package nguyenhuu.hoang.powerstationidentifier.models
 
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.evrencoskun.tableview.adapter.AbstractTableAdapter
+import com.evrencoskun.tableview.adapter.recyclerview.holder.AbstractViewHolder
+import nguyenhuu.hoang.powerstationidentifier.R
+import android.view.LayoutInflater
+
+
+
 /**
  * Created by Hoang on 6/1/2018.
  */
 data class Station(var LineName: String, var Position: String, var Ward: String, var District: String, var Province: String, var Latitude: String, var Longtitude: String, var PowerLevel: String, var Zone: String, var Team: String, var Height: Float, var ColumnType: String, var Box: String, var Note: String)
 data class Distance(var LineName: String, var Position: String, var Distance: Int, var Incremental: Int, var ReversedIncremental: Int)
+
+class TableViewAdapter(val context: Context) : AbstractTableAdapter<String, String, String>(context) {
+    override fun onCreateColumnHeaderViewHolder(parent: ViewGroup?, viewType: Int): AbstractViewHolder {
+        val layout = LayoutInflater.from(context).inflate(R.layout.table_view_column_header_layout,
+                parent, false)
+        // Create a Custom ViewHolder for a Cell item.
+        return ColumnHeaderViewHolder(layout)
+    }
+
+    override fun onBindColumnHeaderViewHolder(holder: AbstractViewHolder?, columnHeaderItemModel: Any?, columnPosition: Int) {
+        val holder = holder as ColumnHeaderViewHolder
+        holder.cellTextview.text = mColumnHeaderItems[columnPosition]
+    }
+
+    override fun onBindRowHeaderViewHolder(holder: AbstractViewHolder?, rowHeaderItemModel: Any?, rowPosition: Int) {
+        val holder = holder as RowHeaderViewHolder
+        holder.cellTextview.text = mRowHeaderItems[rowPosition]
+    }
+
+    override fun onCreateRowHeaderViewHolder(parent: ViewGroup?, viewType: Int): AbstractViewHolder {
+        val layout = LayoutInflater.from(context).inflate(R.layout.table_view_row_header_layout,
+                parent, false)
+        return RowHeaderViewHolder(layout)
+    }
+
+    override fun getCellItemViewType(position: Int): Int {
+        return 0
+    }
+
+    override fun onCreateCellViewHolder(parent: ViewGroup?, viewType: Int): AbstractViewHolder {
+        val layout = LayoutInflater.from(context).inflate(R.layout.table_view_cell_layout,
+                parent, false)
+        // Create a Custom ViewHolder for a Cell item.
+        return CellViewHolder(layout)
+    }
+
+    override fun onCreateCornerView(): View {
+        return LayoutInflater.from(context).inflate(R.layout.table_view_corner_layout, null)
+    }
+
+    override fun onBindCellViewHolder(holder: AbstractViewHolder?, cellItemModel: Any?, columnPosition: Int, rowPosition: Int) {
+        val holder = holder as CellViewHolder
+        holder.cellTextview.text = (cellItemModel as String?)?: ""
+    }
+
+    override fun getColumnHeaderItemViewType(position: Int): Int {
+        return 0
+    }
+
+    override fun getRowHeaderItemViewType(position: Int): Int {
+        return 0
+    }
+
+
+}
+
+class CellViewHolder(itemView: View): AbstractViewHolder(itemView) {
+    var cellTextview: TextView = itemView.findViewById(R.id.cell_data)
+}
+
+class ColumnHeaderViewHolder(itemView: View): AbstractViewHolder(itemView) {
+    var cellTextview: TextView = itemView.findViewById(R.id.column_header_textView)
+}
+
+class RowHeaderViewHolder(itemView: View): AbstractViewHolder(itemView) {
+    var cellTextview: TextView = itemView.findViewById(R.id.row_header_textview)
+}
