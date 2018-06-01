55350ce537d3095e038092bdaa37167eb39769b3
hoangnh <hoangnn@justgola.com>
2018-06-01 10:11:37 +0700

    Tired of creating UI

---
+++ app/src/main/java/nguyenhuu/hoang/powerstationidentifier/BaseActivity.kt
@@ -0,0 +1,34 @@
+package nguyenhuu.hoang.powerstationidentifier
+
+import android.support.v7.app.AppCompatActivity
+import cn.pedant.SweetAlert.SweetAlertDialog
+
+/**
+ * Created by Hoang on 6/1/2018.
+ */
+abstract class BaseActivity : AppCompatActivity() {
+    protected var loadingDialog: SweetAlertDialog? = null
+
+    fun showLoadingDialog() {
+        if (!isFinishing) {
+            if (loadingDialog == null) {
+                loadingDialog = SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE)
+                loadingDialog?.setTitleText(getString(R.string.loading_data))?.setCancelable(false)
+            }
+
+            if (!isShowingLoadingDialog()) {
+                loadingDialog?.show()
+            }
+        }
+    }
+
+    fun hideLoadingDialog() {
+        if (isShowingLoadingDialog()) {
+            loadingDialog?.dismiss()
+        }
+    }
+
+    fun isShowingLoadingDialog(): Boolean {
+        return loadingDialog?.isShowing == true
+    }
+}
