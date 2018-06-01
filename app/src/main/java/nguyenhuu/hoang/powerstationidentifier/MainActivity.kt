0d663c7e27a000d80db0084076cd18b322c73ffb
hoangnh <hoangnn@justgola.com>
2018-06-01 17:55:14 +0700

    Can search by name and position

--- app/src/main/java/nguyenhuu/hoang/powerstationidentifier/MainActivity.kt
+++ app/src/main/java/nguyenhuu/hoang/powerstationidentifier/MainActivity.kt
@@ -3,6 +3,7 @@ package nguyenhuu.hoang.powerstationidentifier
 import android.os.Bundle
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.jakewharton.rxbinding2.widget.textChanges
 import kotlinx.android.synthetic.main.activity_main.*
 import nguyenhuu.hoang.powerstationidentifier.models.Distance
 import nguyenhuu.hoang.powerstationidentifier.models.Station
@@ -38,16 +39,37 @@ class MainActivity : BaseActivity() {
         // display on tableview
         val columnHeaderList = arrayListOf<String>("Đường dây", "Vị trí", "Khoảng cột", "Cộng dồn", "Cộng dồn ngược")
         val rowHeaderList = IntArray(distances.size, { i -> i + 1 }).map { i -> i.toString() }
-        val cellList = initCellList(distances)
+        val cellList = initCellList(distances, "", "")
         val adapter = TableViewAdapter(this)
         table.adapter = adapter
         adapter.setAllItems(columnHeaderList, rowHeaderList, cellList)
+
+        // filter
+        edt_linename.textChanges().skipInitialValue().subscribe { s ->
+            val newCellList = initCellList(distances, s.toString(), edt_distance.text.toString())
+            val newRowHeaderList = IntArray(newCellList.size, { i -> i + 1 }).map { i -> i.toString() }
+            adapter.setAllItems(columnHeaderList, newRowHeaderList, newCellList)
+        }
+
+        edt_position.textChanges().skipInitialValue().subscribe { s ->
+            val newCellList = initCellList(distances, edt_linename.text.toString(), s.toString())
+            val newRowHeaderList = IntArray(newCellList.size, { i -> i + 1 }).map { i -> i.toString() }
+            adapter.setAllItems(columnHeaderList, newRowHeaderList, newCellList)
+        }
     }
 
-    fun initCellList(distances: ArrayList<Distance>): List<List<String>> {
+    fun initCellList(distances: ArrayList<Distance>, name: String, position: String): List<List<String>> {
         val result = emptyArray<List<String>>().toMutableList()
-        distances.forEachIndexed { _, distance ->
-            result.add(arrayOf<String>(distance.LineName, distance.Position, distance.Distance.toString(), distance.Incremental.toString(), distance.ReversedIncremental.toString()).toList())
+        val sName = Utils.removeAccent(name)
+        val sPosition = Utils.removeAccent(position)
+        distances.forEach {
+            var satisfiedDistance: Distance? = null
+            if ((it.LineNameNoAccent.contains(sName, true) || sName.isBlank()) && (it.PositionNoAccent.contains(sPosition, true) || sPosition.isBlank())) {
+                satisfiedDistance = it
+            }
+            if (satisfiedDistance != null) {
+                result.add(arrayOf<String>(satisfiedDistance.LineName, satisfiedDistance.Position, satisfiedDistance.Distance.toString(), satisfiedDistance.Incremental.toString(), satisfiedDistance.ReversedIncremental.toString()).toList())
+            }
         }
         return result
     }
